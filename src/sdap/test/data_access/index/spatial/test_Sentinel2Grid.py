039447e32e8f7a90dcdd90f0e7b211cc81db4645
Thomas Loubrieu <loubrieu@jpl.nasa.gov>
2021-12-22 12:02:20 -0500

    make the sentinel2grid unit test work

--- src/sdap/test/data_access/index/spatial/test_Sentinel2Grid.py
+++ src/sdap/test/data_access/index/spatial/test_Sentinel2Grid.py
@@ -3,11 +3,11 @@ from sdap.data_access.index.spatial import Sentinel2Grid
 
 class Sentinel2GridTestCase(unittest.TestCase):
     def test_get_codes(self):
-        #lat_range = [42.3, 42.35]
-        #lon_range = [-71.34, -71.22]
+        lat_range = [42.3, 42.35]
+        lon_range = [-71.34, -71.22]
 
-        lat_range = [42.303, 43.326]
-        lon_range = [-72.572, -71.183]
+        #lat_range = [42.303, 43.326]
+        #lon_range = [-72.572, -71.183]
 
         i = Sentinel2Grid()
         s2_codes = i.get_codes(lon_range, lat_range)
